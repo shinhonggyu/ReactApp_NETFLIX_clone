75d8143443f04691fab942d77428d7a7afc18fd2
shinhonggyu <zowoz0@naver.com>
2021-01-14 04:07:18 +0900

    Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
 
 const Nav = () => {
-  return <nav>Navbar</nav>;
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${show && 'nav__black'}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt=""
+      />
+      <img className="nav__avatar" src="./img/신홍규.jpg" alt="" />
+    </nav>
+  );
 };
 
 export default Nav;
